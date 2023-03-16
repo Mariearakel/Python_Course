e0a4c7435bdc907fc77fae5400e7e9a72d393927
Mariearakel <mari.arakelyan.1994@gmail.coml>
2023-03-17 01:26:50 +0400

    added homework.py

---
+++ homework.py
@@ -0,0 +1,65 @@
+print("For this challenge, you are supposed to find the sum of the digits of a two-digit number 45 ➞ 9 ,38-11 ,67-13")
+x=4
+y=5
+a=3
+b=8
+c=6
+d=7
+print(a+b,x+y,c+d)
+
+print("A repdigit is a positive number composed out of the same digit. Create a function that takes an two-digit integer and returns whether it's a repdigit or not.44 ➞ True45 ➞ False-44 ➞ False")
+
+x=5
+y=x*11!=1<=x<=9
+print(y)
+
+
+
+print("Write a function that takes an integer minutes and converts it to seconds.5 ➞ 300 2 ➞ 120")
+
+x=60
+z=5
+y=x*z
+print(y)
+x=60
+z=2
+y=x*2
+print(y)
+
+print("Create a function that takes the age in years and returns the age in days. 65 ➞ 23725  0➞ 0 20 ➞ 7300")
+x=365
+z=65
+y=x*z
+a=0
+b=20
+print(x*z,x*a,x*b)
+
+
+print("Write a function that takes two integers (hours, minutes), converts them to seconds, and adds them.1,3 ➞ 3780 2,0 ➞ 7200")
+
+x=3600
+y=60
+z=1
+m=3
+l=(x*z)+(y*m)
+print(l)
+x=3600
+y=60
+z=2
+m=0
+l=(x*z)+(y*m)
+print(l)
+
+print("Create a function that accepts a measurement value in inches and returns the equivalent of the measurement value in feet.")
+
+x=24
+y=x//12
+print(y)
+
+print("Create a function that takes a number as an argument and returns "even" for even numbers and "odd" for odd numbers.")
+
+x = int(("5"))
+if (x% 2) == 0:
+   print("{0} is Even".format(x))
+else:
+   print("{0} is Odd".format(x))
